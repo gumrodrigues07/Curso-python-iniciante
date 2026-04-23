17bcfe77c90c967465013d5bc79499c49079ae26
Gustavo Rodrigues <gumrodrigues07@gmail.com>
2026-04-23 16:03:26 -0300

    aula7

--- Aula7.py
+++ Aula7.py
@@ -1,12 +1,25 @@
-lista_preco = [1500, 1000, 800, 3000]
+lista_preco = [3500, 1000, 800, 3000]
 
 #a1 = 0.2
 #a2 = 0.10
 #a3 = 0.05
 
-for preco in lista_preco:
-    ir = 0.2 * preco
+def calcula_it(preco):
+    if preco <= 2000:
+        ir = 0.2 * preco
+    else:
+        ir = 0.3
     iss = 0.15 * preco
     csll = 0.05 * preco
     imposto_total = ir + iss + csll
+    return imposto_total
+        
+for preco in lista_preco:
+    imposto_total = calcula_it(preco)
     print(f"imposto total sobre o produto de R${preco}: {imposto_total}")
+
+nova_lista_preco = [3000, 23990, 3212, 5109]
+
+for preco in nova_lista_preco:
+    imposto_total = calcula_it(preco)
+    print(f"Imposto total sobre produto R${preco}: {imposto_total}")
